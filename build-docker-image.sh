582dafa450011c03b56508fff8d8f15d3c1b7359
Kasra Rasaee <krasaee@gmail.com>
2023-12-18 14:57:43 -0500

    Update build-docker-image.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker build --platform linux/amd64 -t quantumwake/alethic-ism-core --build-arg GITHUB_REPO_URL=https://github.com/quantumwake/alethic-ism-core.git .
+docker build --platform linux/amd64 -t quantumwake/alethic-ism-core:latest \
+ --build-arg GITHUB_REPO_URL=https://github.com/quantumwake/alethic-ism-core.git .
 
 #docker run -d --name alethic-ism-core-build quantumwake/alethic-ism-core
 
