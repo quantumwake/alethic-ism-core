0e6479e6443c07b517c8ca22a3af3546c171c528
Kasra Rasaee <kasrarasaee@Kasras-MacBook-Pro-2.local>
2023-12-18 16:57:32 -0500

    changes to the extract script, likely not setting the export var and so next step is not picking it up?

--- docker_extract_conda_package.sh
+++ docker_extract_conda_package.sh
@@ -9,9 +9,11 @@ echo "Container ID: $container_id" # For debugging
 
 docker images # Optional: For debugging, to list available images
 file_path=$(docker run --rm --entrypoint find "$image" "$search_path" -name "$search_pattern")
+
 file_name=$(basename $file_path)
 echo "File name: $file_name located in docker image at $file_path" # For debugging
 
+export file_name
 docker cp "$container_id:$file_path" $file_name 
 echo "::set-output name=file_path::$file_path"
 
