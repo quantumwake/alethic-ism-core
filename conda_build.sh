e44576bfaf47eca9e36ee191bba0008f36f3cd32
Kasra Rasaee <krasaee@gmail.com>
2025-02-02 07:04:07 -0800

    fix accidently docker build script issue

--- conda_build.sh
+++ conda_build.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Default values
 META_YAML_PATH="./recipe"
 LOCAL_CHANNEL_PATH=~/miniconda3/envs/local_channel
 SKIP_PURGE=false
-VERBOSE=false
+VERBOSE=true
+USE_BOA=true
 
-# Parse optional arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --meta-yaml-path) META_YAML_PATH="$2"; shift ;;
         --local-channel-path) LOCAL_CHANNEL_PATH="$2"; shift ;;
         --skip-purge) SKIP_PURGE=true ;;
+        --use-boa) USE_BOA=true ;;
         --verbose) VERBOSE=true ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-# Function to log messages if verbose mode is on
 log() {
     if [ "$VERBOSE" = true ]; then
         echo "$@"
@@ -28,16 +27,12 @@ log() {
 log "Using META_YAML_PATH: $META_YAML_PATH"
 log "Using LOCAL_CHANNEL_PATH: $LOCAL_CHANNEL_PATH"
 
-# Update build number
-build_no=$(cat "$META_YAML_PATH/meta.yaml" | awk '/number: [0-9]+/ {printf("%s+1\n", $2)}')
+build_no=$(awk '/number: [0-9]+/ {print $2}' "$META_YAML_PATH/meta.yaml")
 build_no=$((build_no+1))
-log "Updated build number to: $build_no"
-
 sed -E "s/number: [0-9]+/number: $build_no/g" "$META_YAML_PATH/meta.yaml" > ./tmp_meta.yaml
 mv ./tmp_meta.yaml "$META_YAML_PATH/meta.yaml"
-grep "number: " "$META_YAML_PATH/meta.yaml"
+log "Updated build number to: $build_no"
 
-# Purge and clean, unless skipped
 if [ "$SKIP_PURGE" = false ]; then
     log "Purging and cleaning..."
     yes | conda build purge
@@ -46,7 +41,12 @@ else
     log "Skipping purge and clean..."
 fi
 
-# Build to a local channel
-#conda config --set solver classic
-conda build "$META_YAML_PATH" --output-folder "$LOCAL_CHANNEL_PATH"
+if [ "$USE_BOA" = true ]; then
+    log "Using BOA (mamba) for faster builds"
+    boa build "$META_YAML_PATH" --output-folder "$LOCAL_CHANNEL_PATH"
+else
+    log "Using Conda Build"
+    conda build "$META_YAML_PATH" --output-folder "$LOCAL_CHANNEL_PATH" --dirty
+fi
+
 conda index "$LOCAL_CHANNEL_PATH"
